011125c4af6a3d4078cdbbc0f9c2fc7f98e13055
Plan C <hubenchang0515@outlook.com>
2024-08-23 13:46:21 +0800

    调整uri

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
  export const CONFIG = {
-    HOST: `${window.location.origin}/cms`,
+    HOST: `${window.location.origin}/api/1`,
     TYPE_FILTER: ['电影', '连续剧', '综艺', '动漫', '资讯', '公告', '头条', '伦理片'],
 }
