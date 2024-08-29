4d7885438d127aea694c5b1ab8e756837eceddb1
planc <hubenchang0515@outlook.com>
2024-08-29 08:45:35 +0000

    增加 Github 跳转链接

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import 'tdesign-react/es/style/index.css';
-import { Menu } from 'tdesign-react';
-import { HomeIcon } from 'tdesign-icons-react';
+import { Button, Menu, Space } from 'tdesign-react';
+import { HomeIcon, LogoGithubFilledIcon } from 'tdesign-icons-react';
 import SearchBox from './SearchBox';
 
 export interface NavigationProps {
@@ -10,7 +10,12 @@ export interface NavigationProps {
 export default function Navigation(props:NavigationProps) {
     const { HeadMenu, MenuItem } = Menu;
     return (
-            <HeadMenu operations={<SearchBox onSearch={props.onSearch}/>}>
+            <HeadMenu operations={
+                <Space align='center'>
+                    <Button icon={<LogoGithubFilledIcon/>} size="large" variant="text" shape="square" href='https://github.com/hubenchang0515/cinema'/>
+                    <SearchBox onSearch={props.onSearch}/>
+                </Space>
+            }>
                 <MenuItem value={'none'} icon={<HomeIcon />} content="首页" href='/'>
                 </MenuItem> 
             </HeadMenu>
